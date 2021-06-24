c64f2885f8b6cde5c23c389689f51236e691fd75
yanq <yanq@yanqdeMacBook-Pro.local>
2021-06-24 20:05:58 +0800

    first commit

---
+++ config/__init__.py
@@ -0,0 +1,14 @@
+# zoomeye https://www.zoomeye.org/
+zoom_email=""
+zoom_password=""
+
+# fofa https://fofa.so/
+fofa_email=""
+fofa_key=""
+
+# shodan api_key from https://account.shodan.io
+shodao_apikey=""
+
+# censys https://censys.io/account
+censys_uid=""
+censys_secret=""
---
+++ tget.py
@@ -0,0 +1,10 @@
+import requests
+
+from lib.log import set_log
+import time
+
+logger = set_log(__name__)  # 日志方法调用
+
+if __name__ == "__main__":
+    logger.info("测试日志")
+
